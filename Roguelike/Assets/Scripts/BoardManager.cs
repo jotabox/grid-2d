ec5abd3d2ec3b3646d99567d5283f9c2d2946fcb
Jota <jotapaulo.box@gmail.com>
2024-12-18 13:16:03 -0300

    2 commit bordas
    adicionado mais 1 array para conter e selecionar as bordas , e uma verificação dentro do laço for , para saber se é borda ou nao , para colocar os tiles corretos

--- Roguelike/Assets/Scripts/BoardManager.cs
+++ Roguelike/Assets/Scripts/BoardManager.cs
@@ -12,6 +12,7 @@ public class BoardManager : MonoBehaviour
     [SerializeField] private float width;
     [SerializeField] private float height;
     public Tile[] GroundTiles;
+    public Tile[] WallTiles;
 
 
     // Start is called before the first frame update
@@ -28,10 +29,27 @@ public class BoardManager : MonoBehaviour
             {
                 //tilenumber recebe um valor aleatorio dos tiles que estao dentro do array "groundtiles"
                 //cada numero é que vale a uma posição dentro do array
-                int tileNumber = Random.Range(0, GroundTiles.Length);
+                //int tileNumber = Random.Range(0, GroundTiles.Length);
+
+                //variavel que vai setar os tiles
+                Tile tile;
+
+                //Essa condição verifica se a posição atual está nas bordas da matriz
+                if (j == 0 || i == 0 || j == width - 1  || i == height -1)
+                {
+                    //Se a posição está na borda, um tile aleatório é selecionado do array WallTiles para ser usado.
+                    //Isso  representa uma parede ou limite do mapa.
+                    tile = WallTiles[Random.Range(0, WallTiles.Length)];
+                }
+                else
+                {
+                    //Para posições que não estão nas bordas, um tile aleatório é selecionado do array GroundTiles,
+                    //representando o terreno interior.
+                    tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
+                }
 
                 // aqui estamos setando no mapa o tile escolhido aleatorio no tilenumber , nas posiçoes i e j
-                _tilemap.SetTile(new Vector3Int(i,j,0),GroundTiles[tileNumber]);
+                _tilemap.SetTile(new Vector3Int(i,j,0),tile);
             }
         }
     }
